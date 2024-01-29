e49eb156b0ba5f92ad6e88f719bbb9aa9a692309
Zakariae El-Khadir <zel-khad@c4r5p8.1337.ma>
2024-01-29 15:44:38 +0100

    v1

--- so_long/key_press.c
+++ so_long/key_press.c
@@ -2,16 +2,17 @@
 
 int key_press(int keycode, t_data *data)
 {
-    if (keycode == 53)
+    printf("the key |%i|\n",keycode);
+    if (keycode == 65307)
         exit(0);
 
-    if (keycode == 123) 
+    if (keycode == 65361 ||keycode == 97) 
         data->player_x -= 10;
-    else if (keycode == 124) 
+    else if (keycode == 65363 ||keycode == 100) 
         data->player_x += 10;
-    else if (keycode == 125) 
+    else if (keycode == 65364 ||keycode == 115) 
         data->player_y += 10;
-    else if (keycode == 126) 
+    else if (keycode == 65362 ||keycode == 119) 
         data->player_y -= 10;
 
     mlx_clear_window(data->mlx, data->win);
--- so_long/pixel_put.c
+++ so_long/pixel_put.c
@@ -2,5 +2,10 @@
 
 void draw_player(t_data *data)
 {
-    mlx_pixel_put(data->mlx, data->win, data->player_x, data->player_y, 0x000000FF);
+	void	*img;
+	// char	*relative_path;
+	int		img_width = 50;
+	int		img_height = 50;
+    // mlx_pixel_put(data->mlx, data->win, data->player_x, data->player_y, 0x000000FF);
+    img = mlx_png_file_to_image(data->mlx, "/nfs/homes/zel-khad/so_long/so_long/pacman.png",  &img_width, &img_height);
 }
