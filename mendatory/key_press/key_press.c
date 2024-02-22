3a9cb46f5b305c3bafe3a29f4765a51737a57c8d
zel-khad <zel-khad@student.42.fr>
2024-02-22 11:58:51 +0100

    mac

---
+++ mendatory/key_press/key_press.c
@@ -0,0 +1,40 @@
+#include "../so_long.h"
+
+int	handle_destroy_notify(t_data *data)
+{
+	write(1, ANSI_COLOR_YELLOW "THE GMAE IS OVER", 21);
+	mlx_destroy_window(data->mlx, data->win);
+	free_(data->map);
+	free(data->mlx);
+	exit(1);
+}
+
+int	key_hook(int keysym, t_data *data)
+{
+	if (keysym == 53)
+	{
+		write(1, ANSI_COLOR_YELLOW "THE GMAE IS OVER", 21);
+		mlx_destroy_window(data->mlx, data->win);
+		free_(data->map);
+		free(data->mlx);
+        system("leaks so_long");
+		exit(1);
+	}
+	if (keysym == 123 || keysym == 0)
+	{
+		move_player(data, data->player_x, data->player_y - 1);
+	}
+	else if (keysym == 124 || keysym == 2)
+	{
+		move_player(data, data->player_x, data->player_y + 1);
+	}
+	else if (keysym == 125 || keysym == 1)
+	{
+		move_player(data, data->player_x + 1, data->player_y);
+	}
+	else if (keysym == 126 || keysym == 13)
+	{
+		move_player(data, data->player_x - 1, data->player_y);
+	}
+	return (0);
+}
---
+++ mendatory/so_long.c
@@ -0,0 +1,26 @@
+
+#include "so_long.h"
+
+int	main(int ac, char **av)
+{
+	t_data	data;
+
+	if (ac != 2)
+		return (write(1, ANSI_COLOR_YELLOW "ENTRER UNE MAP .. !", 24));
+	if (read_map(&data, av[1]) == 0)
+		return (0);
+	ft_position_player(data.map, &data.player_x, &data.player_y);
+	if (parssin_map(&data, data.tmp_map) == 0)
+		return (free_(data.map), EXIT_FAILURE);
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, count_map_colone(data.map) * 42,
+			count_map_line(data.map) * 42, "SO_LONG");
+	draw_player(&data);
+	mlx_key_hook(data.win, key_hook, &data);
+	mlx_hook(data.win, 17, 0, handle_destroy_notify, &data);
+	mlx_loop(data.mlx);
+	mlx_destroy_window(data.mlx, data.win);
+	free_(data.map);
+	free_(data.tmp_map);
+	return (EXIT_SUCCESS);
+}
