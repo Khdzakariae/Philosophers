58553c0778b65d6189afb12856294267a74c0cf3
zel-khad <elkhadirzakaria48@gmail.com>
2024-05-26 17:45:50 +0100

    Fix: Join threads function with proper parameters
    Refactors the join_threads function to use proper variable names and corrects the parameter list.

--- philo_nv/sources/philosophers.c
+++ philo_nv/sources/philosophers.c
@@ -1,45 +1,40 @@
 #include "philo.h"
 
-
-void join_threads(t_data *data, t_philo *philos) 
+void	join_threads(t_data *data, t_philo *philos)
 {
-    (void)*data;
-    for (int i = 0; i < philos->data->number_of_philosophers  ; i++) 
-    {
-        // puts("======= bata =======");
-        pthread_join(philos[i].thread_philo, NULL);
-        // puts("======= allo =======");
-
-
-    }
+	(void)*data;
+	for (int i = 0; i < philos->data->number_of_philosophers; i++)
+	{
+		// puts("======= bata =======");
+		pthread_join(philos[i].thread_philo, NULL);
+		// puts("======= allo =======");
+	}
 }
 
-int main(int argc, char **argv) 
+int	main(int argc, char **argv)
 {
+	t_data data;
+	t_forks *forks;
+	t_philo *philo;
+	data.philosopher_died = false;
+	if (check_arguments(argc, argv, &data) != 0)
+		return (1);
+	forks = initialize_forks(data.number_of_philosophers);
+	if (forks == NULL)
+		return (1);
+	// pthread_mutex_init(&data._died, NULL);
+	philo = initialize_philosophers(&data, forks);
+	start_simulation(&data, philo);
+	if (monitoring(philo) == false)
+	{
+		if (data.number_of_philosophers != 1)
+			join_threads(&data, philo);
+		// pthread_join(philos.thread_philo, NULL);
 
-    t_data data;
-    t_forks *forks;
-    t_philo *philo;
-    data.philosopher_died = false;
-    if (check_arguments(argc, argv, &data) != 0)
-        return 1;
-    forks = initialize_forks(data.number_of_philosophers);
-    if (forks == NULL)
-        return 1;
-    // pthread_mutex_init(&data._died, NULL);
-    philo = initialize_philosophers(&data, forks);
-    start_simulation(&data, philo);
-    if  (monitoring(philo)== false) 
-    {
-        if (data.number_of_philosophers != 1)
-            join_threads(&data, philo);
-        // pthread_join(philos.thread_philo, NULL);
-        
-        cleanup(philo, forks, data.number_of_philosophers);
-        return 1;
-    }
+		cleanup(philo, forks, data.number_of_philosophers);
+		return (1);
+	}
 
-
-    cleanup(philo, forks, data.number_of_philosophers);
-    return(0);
+	cleanup(philo, forks, data.number_of_philosophers);
+	return (0);
 }
