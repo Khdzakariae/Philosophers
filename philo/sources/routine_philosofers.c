ae77983434277536d3a76b1b60b74ddb2173d445
zel-khad <elkhadirzakaria48@gmail.com>
2024-05-27 18:56:22 +0100

    Refactor: Update Makefile, code formatting, and source files
    - Updated Makefile with correct indentation and build flags
    - Applied consistent code formatting to improve readability
    - Added missing files to the .gitignore file
    - Refactored source files to enhance code organization and clarity

---
+++ philo/sources/routine_philosofers.c
@@ -0,0 +1,64 @@
+#include "philo.h"
+
+bool	monitoring(t_philo *philos)
+{
+	int	i;
+
+	while (1)
+	{
+		i = 0;
+		while (i < philos->data->number_of_philosophers)
+		{
+			if (cheack_time_died(philos, i) == false)
+			{
+				set_philo_died(philos);
+				print_msg(3, &philos[i], false);
+				return (false);
+			}
+			i++;
+		}
+	}
+	return (true);
+}
+
+void	*philosophers(void *arg)
+{
+	t_philo	*philo;
+	int		i;
+
+	philo = (t_philo *)arg;
+
+	while (1)
+	{
+		if (cheaak_died(philo) == false)
+			break ;
+		thinking(philo);
+		pthread_mutex_lock(philo->first_fork->forks);
+		print_msg(0, philo, true);
+		pthread_mutex_lock(philo->second_fork->forks);
+		print_msg(0, philo, true);
+		print_msg(4, philo, true);
+		ft_usleep(philo->data->time_to_eat);
+		set_time(philo, i);
+		pthread_mutex_unlock(philo->second_fork->forks);
+		pthread_mutex_unlock(philo->first_fork->forks);
+		sleping(philo);
+		i++;
+	}
+	
+	return (NULL);
+}
+
+void	start_simulation(t_data *data, t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	start_time(true);
+	data->start_time = the_time();
+	while (i < data->number_of_philosophers)
+	{
+		pthread_create(&philos[i].thread_philo, NULL, philosophers, &philos[i]);
+		i++;
+	}
+}
---
+++ philo/sources/set_mutex.c
@@ -0,0 +1,44 @@
+
+
+#include "philo.h"
+
+void	set_philo_died(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->_died);
+	philo->data->philosopher_died = true;
+	pthread_mutex_unlock(&philo->data->_died);
+}
+
+bool	cheaak_died(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->_died);
+	if (philo->data->philosopher_died == true)
+	{
+		pthread_mutex_unlock(&philo->data->_died);
+		return (false);
+	}
+	pthread_mutex_unlock(&philo->data->_died);
+	return (true);
+}
+
+void	set_time(t_philo *philo, int i)
+{
+	pthread_mutex_lock(&philo->data->_hbsso_l9lawi);
+	(philo)->time_to_last_eat = the_time();
+	pthread_mutex_unlock(&philo->data->_hbsso_l9lawi);
+}
+
+bool	cheack_time_died(t_philo *philos, int i)
+{
+	pthread_mutex_lock(&philos->data->_hbsso_l9lawi);
+	long current_time = the_time();
+	long dure = current_time - philos->time_to_last_eat;
+	// sleep(10);
+	if (dure >= philos->data->time_to_die)
+	{
+		pthread_mutex_unlock(&philos->data->_hbsso_l9lawi);
+		return (false);
+	}
+	pthread_mutex_unlock(&philos->data->_hbsso_l9lawi);
+	return (true);
+}
