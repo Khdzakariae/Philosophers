44d3a29b8d327038e4f640454bc0a5eeb1cb406f
zel-khad <elkhadirzakaria48@gmail.com>
2024-05-27 19:06:38 +0100

    Refactor: Add missing includes to philo_bonus.h

--- philo_bonus/includes/philo_bonus.h
+++ philo_bonus/includes/philo_bonus.h
@@ -1,6 +1,24 @@
 #ifndef PHILO_BONNUS_H
 # define PHILO_BONNUS_H
 
-#include <stdio.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <time.h>
+# include <unistd.h>
 
+typedef struct s_data
+{
+	long			number_of_philosophers;
+	long			time_to_died;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+
+}					t_data;
+
+int	check_arguments(int argc, char **argv, t_data *data);
 #endif
