d6743d5cbf562b44558b704acdd7d9404df89726
Khdzakariae <elkhadirzakaria48@gmail.com>
2024-05-27 11:35:17 +0100

    Refactor: Remove debug prints and unused mutex

--- philo_nv/sources/philosophers.c
+++ philo_nv/sources/philosophers.c
@@ -5,9 +5,7 @@ void	join_threads(t_data *data, t_philo *philos)
 	(void)*data;
 	for (int i = 0; i < philos->data->number_of_philosophers; i++)
 	{
-		// puts("======= bata =======");
 		pthread_join(philos[i].thread_philo, NULL);
-		// puts("======= allo =======");
 	}
 }
 
@@ -22,15 +20,12 @@ int	main(int argc, char **argv)
 	forks = initialize_forks(data.number_of_philosophers);
 	if (forks == NULL)
 		return (1);
-	// pthread_mutex_init(&data._died, NULL);
 	philo = initialize_philosophers(&data, forks);
 	start_simulation(&data, philo);
 	if (monitoring(philo) == false)
 	{
 		if (data.number_of_philosophers != 1)
 			join_threads(&data, philo);
-		// pthread_join(philos.thread_philo, NULL);
-
 		cleanup(philo, forks, data.number_of_philosophers);
 		return (1);
 	}
