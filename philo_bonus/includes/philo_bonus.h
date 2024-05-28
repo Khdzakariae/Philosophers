46be8f0fac24a1c92789c1e2b6e8ad713664b9b3
zel-khad <elkhadirzakaria48@gmail.com>
2024-05-28 18:53:29 +0100

    Refactor: Introduce philosopher struct and initialization function

--- philo_bonus/includes/philo_bonus.h
+++ philo_bonus/includes/philo_bonus.h
@@ -17,8 +17,20 @@ typedef struct s_data
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
+	long			must_eat;
 
 }					t_data;
 
+typedef struct s_philo
+{
+	int				pid;
+	long			id;
+	long			time_to_last_eat;
+	long			cont;
+	t_data			*data;
+}					t_philo;
+
+
+t_philo  *initialize_philosophers(t_data *data);
 int	check_arguments(int argc, char **argv, t_data *data);
 #endif
