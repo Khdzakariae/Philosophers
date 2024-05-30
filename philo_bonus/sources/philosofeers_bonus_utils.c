64708814c3f9c81da10acd365d8988a57ec02fc1
zel-khad <elkhadirzakaria48@gmail.com>
2024-05-30 12:40:22 +0100

    Fix: Implement philosophers thinking and exit routines
    Adds the philosophers' thinking and exit routines to the simulation.

--- philo_bonus/sources/philosofeers_bonus_utils.c
+++ philo_bonus/sources/philosofeers_bonus_utils.c
@@ -1,5 +1,13 @@
 #include <philo_bonus.h>
 
+void retine(t_philo *philo)
+{
+    sem_wait(philo->data->semaphore);
+    thinking(philo);
+    sleep(1);
+    sem_post(philo->data->semaphore);
+    exit(0);
+}
 
 t_philo  *initialize_philosophers(t_data *data)
 {
@@ -9,6 +17,8 @@ t_philo  *initialize_philosophers(t_data *data)
     philo = malloc((data->number_of_philosophers) * sizeof(t_philo));
     if (philo == NULL)
         return NULL;
+    sem_unlink("/mysemaphore");
+    data->semaphore = (sem_open("/mysemaphore", O_CREAT, 0644, 1));
     while (i < data->number_of_philosophers)
     {
         philo[i].id = i;
@@ -18,4 +28,23 @@ t_philo  *initialize_philosophers(t_data *data)
         i++;
     }
     return(philo);   
+}
+
+
+void  start_simulation(t_data *data, t_philo *philo)
+{
+    int i = 0;
+    int pid = 0;
+	start_time(true);
+	data->start_time = the_time();
+    while (i < data->number_of_philosophers)
+    {
+        philo[i].pid = fork();
+        if (philo[i].pid == 0)
+            retine(&philo[i]);
+        else
+            i++;
+    }
+    for (int j = 0; j <  data->number_of_philosophers; j++)
+        wait(NULL);
 }
