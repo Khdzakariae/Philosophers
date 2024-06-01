936ca53dbdefefaf110fd0dab87e1a8ac248e658
zel-khad <elkhadirzakaria48@gmail.com>
2024-06-01 22:54:00 +0100

    Fix: Add logs to set_time() for debugging

--- philo_bonus/sources/set.c
+++ philo_bonus/sources/set.c
@@ -13,7 +13,12 @@ void arrete(t_philo *philo)
 
 void	set_time(t_philo *philo)
 {
+	// puts("------------------ ENTRER ----------------------------");
 	philo->time_to_last_eat = the_time();
+	printf("philo id is : %ld\n", philo->id);
+	printf("----------philo lat eat  is : %ld--------\n", philo->time_to_last_eat);
+	// puts("-------------------- SORTIR --------------------------");
+
 }
 
 bool	cheack_time_died(t_philo *philos, int i)
@@ -21,6 +26,7 @@ bool	cheack_time_died(t_philo *philos, int i)
 
 	long current_time = the_time();
 	long dure = current_time - philos->time_to_last_eat;
+
 	if (dure > philos->data->time_to_die)
 		return (false);
 	return (true);
@@ -31,24 +37,26 @@ void	set_philo_died(t_philo *philo)
 	philo->data->philosopher_died = true;
 }
 
-bool	monitoring(t_philo *philos , int ac)
+bool monitoring(t_philo *philos, int ac)
 {
-	int	i;
+    int i;
+    bool all_alive = true;
 
-	while (1)
-	{
-		i = 0;
-		while (i < philos->data->number_of_philosophers)
-		{
-			if (cheack_time_died(philos, i) == false)
-			{
-				set_philo_died(philos);
-				arrete(philos);
-				print_msg(3, &philos[i], false);
-				exit(1);
-			}
-			i++;
-		}
-	}
-	return (true);
+    while (all_alive)
+    {
+        i = 0;
+        while (i < philos->data->number_of_philosophers)
+        {
+            if (!cheack_time_died(&philos[i], i))
+            {
+                set_philo_died(philos);
+                arrete(philos);
+                print_msg(3, &philos[i], false);
+                all_alive = false; 
+                break;
+            }
+            i++;
+        }
+    }
+    return (true);
 }
