ee152c0032cd9421072fa558399880b219eb9b46
Khdzakariae <elkhadirzakaria48@gmail.com>
2024-06-02 19:52:11 +0100

    Refactor: Add cleanup_forks function
    Added a cleanup_forks function to correctly destroy and free memory allocated for forks.

--- lat_philo_linux/philo/sources/philosofeers_utils.c
+++ lat_philo_linux/philo/sources/philosofeers_utils.c
@@ -1,4 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosofeers_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 18:08:54 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/06/02 19:03:24 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+
+void cleanup_forks(t_forks *forks, int j)
+{
+	int i = 0;
+	while (i < j)
+	{
+		pthread_mutex_destroy(forks[i].forks);
+		free(forks[i].forks);
+		i++;
+	}
+	free(forks);
+}
 
 void	cleanup(t_philo *philo, t_forks *forks, int number_of_philosophers)
 {
@@ -11,6 +35,11 @@ void	cleanup(t_philo *philo, t_forks *forks, int number_of_philosophers)
 		free(forks[i].forks);
 		i++;
 	}
+	// pthread_mutex_destroy(&philo->data->cont_mutix);
+	// pthread_mutex_destroy(&philo->data->print_mutex);
+	// pthread_mutex_destroy(&philo->data->_died);
+	// pthread_mutex_destroy(&philo->data->set_time_mutix);
+
 	free(philo);
 	free(forks);
 }
@@ -51,11 +80,16 @@ t_forks	*initialize_forks(int number_of_philosophers)
 
 	i = 0;
 	forks = malloc((number_of_philosophers) * sizeof(t_forks));
-	// cheack is fail allocation
+	if (forks == NULL)
+		return(NULL);
 	while (i < number_of_philosophers)
 	{
 		forks[i].forks = malloc(sizeof(pthread_mutex_t));
-		// cheack is fail allocation
+		if (forks[i].forks == NULL)
+		{
+			cleanup_forks(forks, i);
+			return(NULL);
+		}
 		pthread_mutex_init(forks[i].forks, NULL);
 		i++;
 	}
@@ -64,14 +98,18 @@ t_forks	*initialize_forks(int number_of_philosophers)
 
 t_philo	*initialize_philosophers(t_data *data, t_forks *forks)
 {
-	int i = 0;
-	t_philo *philos = malloc((data->number_of_philosophers) * sizeof(t_philo));
+	t_philo *philos ;
+	int nbr;
+	int i ;
+
+	i = 0;
+	philos = malloc((data->number_of_philosophers) * sizeof(t_philo));
 
 	pthread_mutex_init(&data->cont_mutix, NULL);
 	pthread_mutex_init(&data->print_mutex, NULL);
 	pthread_mutex_init(&data->_died, NULL);
-	pthread_mutex_init(&data->_hbsso_l9lawi, NULL);
-	int nmbr = data->number_of_philosophers;
+	pthread_mutex_init(&data->set_time_mutix, NULL);
+	nbr = data->number_of_philosophers;
 
 	while (i < data->number_of_philosophers)
 	{
@@ -79,22 +117,8 @@ t_philo	*initialize_philosophers(t_data *data, t_forks *forks)
 		philos[i].cont = 0;
 		philos[i].data = data;
 		philos[i].time_to_last_eat = 0;
-		pthread_mutex_init(&philos[i].time_mutex, NULL);
-		philos[i].first_fork = &forks[(i + 1) % nmbr];
+		philos[i].first_fork = &forks[(i + 1) % nbr];
 		philos[i].second_fork = &forks[i];
-		// if (i == 0)
-		// {
-		// 	philos[i].first_fork = &forks[data->number_of_philosophers - 1];
-		// 	philos[i].second_fork = &forks[0];
-		// }
-		// else if (i % 2)
-		// {
-		// 	philos[i].first_fork = &forks[i];
-		// 	philos[i].second_fork = &forks[(i - 1) % nmbr];
-		// }
-		// else
-		// {
-		// }
 		i++;
 	}
 	return (philos);
