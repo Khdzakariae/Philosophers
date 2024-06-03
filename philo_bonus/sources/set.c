cad9b713f6db19919f9ff43ce09e79990daaa1a2
Khdzakariae <elkhadirzakaria48@gmail.com>
2024-06-03 15:23:59 +0100

    Refactor: Move cheaak_died function to set.c

--- philo_bonus/sources/set.c
+++ philo_bonus/sources/set.c
@@ -1,15 +1,13 @@
 #include <philo_bonus.h>
 
 
-
-void	set_time(t_philo *philo)
+bool	cheaak_died(t_philo *philo)
 {
-	// puts("------------------ ENTRER ----------------------------");
-	philo->time_to_last_eat = the_time();
-	// printf("philo id is : %ld\n", philo->id);
-	// printf("----------philo lat eat  is : %ld--------\n", philo->time_to_last_eat);
-	// puts("-------------------- SORTIR --------------------------");
-
+	if (philo->data->philosopher_died == true)
+	{
+		return (false);
+	}
+	return (true);
 }
 
 bool	cheack_time_died(t_philo *philos, int i)
@@ -28,24 +26,3 @@ void	set_philo_died(t_philo *philo)
 	philo->data->philosopher_died = true;
 }
 
-bool monitoring(t_philo *philos, int ac)
-{
-    int i;
-    bool all_alive = true;
-
-    while (all_alive)
-    {
-        i = 0;
-        while (i < philos->data->number_of_philosophers)
-        {
-            if (!cheack_time_died(&philos[i], i))
-            {
-                arrete(philos);
-                all_alive = false; 
-                break;
-            }
-            i++;
-        }
-    }
-    return (true);
-}
