3489a250b732010eeecb59115cc08f742fe77355
Zakariae El-Khadir <zel-khad@c3r8p9.1337.ma>
2024-07-04 18:55:55 +0100

    Fix: Update year in header comments
    This commit updates the year in the header comments of philo_bonus/sources/philosopuers_bonus.c to the current year, 2023, to reflect the correct timeline.

--- philo_bonus/sources/philosopuers_bonus.c
+++ philo_bonus/sources/philosopuers_bonus.c
@@ -1,13 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopuers_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 18:41:56 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/07/04 18:41:57 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <philo_bonus.h>
-
-
 
 int	main(int argc, char **argv)
 {
 	t_data	data;
 	t_philo	*philo;
-
 
 	if (check_arguments(argc, argv, &data) != 0)
 		return (1);
