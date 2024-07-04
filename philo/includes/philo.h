5215d5eb46af9f15d2baddef4868453126db7d42
Zakariae El-Khadir <zel-khad@c3r8p9.1337.ma>
2024-07-04 18:54:59 +0100

    Refactor: Migrate time-related functions to philo.h
    Migrates functions related to time management (e.g., `cheack_time_died`, `set_time`) into the `philo.h` header file, enhancing code organization and clarity.

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 18:09:19 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/06/03 09:55:23 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
@@ -23,7 +35,7 @@ typedef struct s_data
 	pthread_mutex_t	cont_mutix;
 	pthread_mutex_t	print_mutex;
 	pthread_mutex_t	_died;
-	pthread_mutex_t	_hbsso_l9lawi;
+	pthread_mutex_t	set_time_mutix;
 
 }					t_data;
 
@@ -40,31 +52,40 @@ typedef struct s_philo
 	t_data			*data;
 	t_forks			*first_fork;
 	t_forks			*second_fork;
-	pthread_mutex_t	time_mutex;
 	pthread_t		thread_philo;
 
 }					t_philo;
 
-bool				monitoring_cont(t_philo *philos);
-void 				set_cont(t_philo *philo);
-bool 				cheack_cont(t_philo *philo);
-bool				cheack_time_died(t_philo *philos, int i);
-void				set_time(t_philo *philo);
-void				set_philo_died(t_philo *philo);
 bool				cheaak_died(t_philo *philo);
-bool	monitoring(t_philo *philos , int ac);
+bool				cheack_cont(t_philo *philo);
+bool				cheack_time_died(t_philo *philos);
 
-void				sleping(t_philo *philo);
-void				thinking(t_philo *philo);
-void				ft_usleep(long time);
-long				the_time(void);
-void				print_msg(int flag, t_philo *philo, bool flage);
-long				start_time(bool init);
-void				start_simulation(t_data *data, t_philo *philos);
+int					ft_isdigit(int c);
+void				generating_number(char **str, long *result);
+long				ft_atoi(char *str);
+bool				cheack(t_data *data, char **av, int argc);
+int					check_arguments(int argc, char **argv, t_data *data);
+
+void				cleanup_forks(t_forks *forks, int j);
 void				cleanup(t_philo *philo, t_forks *forks,
 						int number_of_philosophers);
+void				print_msg(int flag, t_philo *philo, bool flage);
 t_forks				*initialize_forks(int number_of_philosophers);
 t_philo				*initialize_philosophers(t_data *data, t_forks *forks);
-int					check_arguments(int argc, char **argv, t_data *data);
+
+void				join_threads(t_philo *philos);
+bool				monitoring(t_philo *philos, int ac);
+void				*philosophers(void *arg);
+void				start_simulation(t_data *data, t_philo *philos);
+
+void				set_philo_died(t_philo *philo);
+void				set_cont(t_philo *philo);
+void				set_time(t_philo *philo);
+
+long				start_time(bool init);
+long				the_time(void);
+void				ft_usleep(long time);
+void				sleping(t_philo *philo);
+void				thinking(t_philo *philo);
 
 #endif
