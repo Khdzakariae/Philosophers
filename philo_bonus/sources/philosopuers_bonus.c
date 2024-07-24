d50ac8984d6e495550c8e2f9fe65d052ca416f33
Zakariae El-Khadir <zel-khad@c3r9p7.1337.ma>
2024-07-24 19:39:22 +0100

    Fix: Handle process exiting for all philosophers

--- philo_bonus/sources/philosopuers_bonus.c
+++ philo_bonus/sources/philosopuers_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:41:56 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/04 18:41:57 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:38:41 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,40 @@ int	main(int argc, char **argv)
 	if (philo == NULL)
 		return (1);
 	start_simulation(&data, philo);
+	
+	while (1) {
+
+	int status;
+	int ret = waitpid(-1, &status, 0);
+	if (ret == -1) 
+		{
+		
+    sem_close(philo->data->forks_semaphore);
+    sem_close(philo->data->cont_semaphore);
+    sem_close(philo->data->semaphore_print);
+    sem_close(philo->data->semaphore_died);
+    sem_close(philo->data->protect_last_eat);
+				free(philo);
+			exit(122);
+		}
+
+    	// if (WIFEXITED(status)) {
+		if (WEXITSTATUS(status) == 0) {
+				continue;
+		} 
+		else if (WEXITSTATUS(status) != 0) {
+				arrete(philo);
+				print_msg(3, &philo[WEXITSTATUS(status)  - 1], false);
+
+	    sem_close(philo->data->forks_semaphore);
+    sem_close(philo->data->cont_semaphore);
+    sem_close(philo->data->semaphore_print);
+    sem_close(philo->data->semaphore_died);
+    sem_close(philo->data->protect_last_eat);
+				free(philo);
+
+				// exit(10);
+		}
+	}
+	
 }
