3a4d2c341f7362f55c2226813d52a536fcf6fd04
Zakariae El-Khadir <zel-khad@c3r7p6.1337.ma>
2024-07-29 11:51:39 +0100

    Refactor: Update printing mechanism and add death flag
    - Removed the `arrete()` function as it is no longer needed.
    - Updated `print_msg()` to handle printing death messages correctly.
    - Added a `data->philosopher_died` flag to track deaths.

--- philo_bonus/sources/philosofeers_bonus_utils.c
+++ philo_bonus/sources/philosofeers_bonus_utils.c
@@ -6,23 +6,11 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:41:50 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/24 19:36:04 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/29 11:49:49 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bonus.h>
-
-void	arrete(t_philo *philo)
-{
-	int		i;
-		i = 0;
-		while (i < philo->data->number_of_philosophers)
-		{
-			// printf("the pis is --> |%d|\n", philo[i].pid);
-			kill(philo[i].pid, SIGKILL);
-			i++;
-		}
-}
 
 void	print_msg(int flag, t_philo *philo, bool flage)
 {
@@ -32,7 +20,6 @@ void	print_msg(int flag, t_philo *philo, bool flage)
 	time = the_time();
 	if (flage == false)
 	{
-		// puts("================");
 		printf("%lld\t%ld died\n", time, philo->id + 1);
 		sem_post(philo->data->semaphore_print);
 		return;
@@ -66,12 +53,15 @@ t_philo	*initialize_philosophers(t_data *data)
 	sem_unlink("/semaphore_print");
 	sem_unlink("/semaphore_died");
 	sem_unlink("/protect_last_eat");
+	sem_unlink("/maaat");
 	data->forks_semaphore = (sem_open("/forks_semaphore", O_CREAT, 0644,
 				data->number_of_philosophers));
 	data->cont_semaphore = (sem_open("/cont_semaphore", O_CREAT, 0644, 1));
 	data->semaphore_print = (sem_open("/semaphore_print", O_CREAT, 0644, 1));
 	data->semaphore_died = (sem_open("/semaphore_died", O_CREAT, 0644, 1));
 	data->protect_last_eat = (sem_open("/protect_last_eat", O_CREAT, 0644, 1));
+	data->maaat = (sem_open("/maaat", O_CREAT, 0644, 1));
+	data->philosopher_died = false;
 	data->cont = 0;
 	while (i < data->number_of_philosophers)
 	{
