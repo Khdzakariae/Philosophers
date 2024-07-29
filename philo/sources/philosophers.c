cc0b00531f36989111a78c9bdc000e486aefdc86
Zakariae El-Khadir <zel-khad@c3r6p1.1337.ma>
2024-07-29 21:29:41 +0100

    Refactor: Introduce new 'start_simulation' function for cleaner main function

--- philo/sources/philosophers.c
+++ philo/sources/philosophers.c
@@ -6,11 +6,25 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 18:09:00 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/22 19:35:02 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/29 21:27:11 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+void	start_simulation(t_data *data, t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	start_time(true);
+	data->start_time = the_time();
+	while (i < data->number_of_philosophers)
+	{
+		pthread_create(&philos[i].thread_philo, NULL, philosophers, &philos[i]);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
