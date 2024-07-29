3058d271443103d9c1d82d83deb9c54d84ef8f9c
Zakariae El-Khadir <zel-khad@c3r7p6.1337.ma>
2024-07-29 12:45:34 +0100

    Refactor: Adjust monitoring thread to handle philosopher death

--- philo_bonus/sources/retine_Philosophers.c
+++ philo_bonus/sources/retine_Philosophers.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:42:00 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/29 11:48:12 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/29 12:35:06 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,21 @@ void	join_threads(t_philo *philos)
 void	*monitoring(void *arg)
 {
 	t_philo	*philo;
-	philo = (t_philo *)arg;
 
-	while (1)
+	philo = (t_philo *)arg;
+	while (cheack_time_died(philo) == true)
 	{
 		if (philo->data->arg == 6 && cheack_cont(philo) == false)
 		{
-			pthread_join(philo->thread_philo, NULL);
-			exit(0);
+			sem_wait(philo->data->chbaa3);
+			sem_wait(philo->data->semaphore_print);
+			break ;
 		}
-		if (cheack_time_died(philo) == false)
-		{
-			set_philo_died(philo);
-			sem_wait(philo->data->maaat);
-			print_msg(0, philo,false);
-		}
-		usleep(100);
+		usleep(200);
 	}
+	set_philo_died(philo);
+	sem_wait(philo->data->maaat);
+	print_msg(15, philo, false);
 	exit(0);
 }
 
@@ -51,12 +49,8 @@ void	retine(t_philo *philo)
 {
 	if (philo->id % 2 != 0)
 		usleep(100);
-	while (1)
+	while (cheack_time_died(philo) == true)
 	{
-		if(cheaak_died(philo) == false)
-			exit(0);
-		if (philo->data->arg == 6 && cheack_cont(philo) == false)
-			exit(0);
 		thinking(philo);
 		sem_wait(philo->data->forks_semaphore);
 		print_msg(0, philo, true);
@@ -72,7 +66,6 @@ void	retine(t_philo *philo)
 		ft_usleep(philo->data->time_to_sleep);
 		usleep(300);
 	}
-
 }
 
 void	start_simulation(t_data *data, t_philo *philo)
@@ -85,7 +78,7 @@ void	start_simulation(t_data *data, t_philo *philo)
 	{
 		philo[i].pid = fork();
 		if (philo[i].pid == 0)
-		{	
+		{
 			pthread_create(&philo[i].thread_philo, NULL, monitoring, &philo[i]);
 			retine(&philo[i]);
 		}
