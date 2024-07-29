3008b1b3b142d87548141510843562cd204133ac
Zakariae El-Khadir <zel-khad@c3r7p6.1337.ma>
2024-07-29 12:45:31 +0100

    Fix: Update seminar name in monitor function
    Improves: Fixes the name of the semaphore used to monitor philosophers' status.

--- philo_bonus/sources/philosopuers_bonus.c
+++ philo_bonus/sources/philosopuers_bonus.c
@@ -6,40 +6,47 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:41:56 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/07/29 11:49:29 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/07/29 12:19:36 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bonus.h>
 
-void main_monitor(t_philo *philo)
+void	main_monitor(t_philo *philo)
 {
 	while (1)
 	{
-		if(philo->data->maaat->__align == 0)
+		if (philo->data->chbaa3->__align == 0)
 		{
-			break;
+			arrete(philo);
+			exit(0);
+			break ;
+		}
+		if (philo->data->maaat->__align == 0)
+		{
+			break ;
 		}
 	}
 }
 
 void	arrete(t_philo *philo)
 {
-		int		i;
-		i = 0;
-		while (i < philo->data->number_of_philosophers)
-		{
-			kill(philo[i].pid, SIGKILL);
-			i++;
-		}
-		sem_close(philo->data->forks_semaphore);
-   		sem_close(philo->data->cont_semaphore);
-   	 	sem_close(philo->data->semaphore_print);
-    	sem_close(philo->data->semaphore_died);
-    	sem_close(philo->data->protect_last_eat);
-    	sem_close(philo->data->maaat);
-		free(philo);
-		exit(1);
+	int	i;
+
+	i = 0;
+	while (i < philo->data->number_of_philosophers)
+	{
+		kill(philo[i].pid, SIGKILL);
+		i++;
+	}
+	sem_close(philo->data->forks_semaphore);
+	sem_close(philo->data->cont_semaphore);
+	sem_close(philo->data->semaphore_print);
+	sem_close(philo->data->semaphore_died);
+	sem_close(philo->data->protect_last_eat);
+	sem_close(philo->data->maaat);
+	sem_close(philo->data->chbaa3);
+	free(philo);
 }
 
 int	main(int argc, char **argv)
@@ -55,5 +62,6 @@ int	main(int argc, char **argv)
 	start_simulation(&data, philo);
 	main_monitor(philo);
 	arrete(philo);
+	exit(1);
 	return (0);
 }
